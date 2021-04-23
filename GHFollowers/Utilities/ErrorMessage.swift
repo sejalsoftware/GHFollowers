f3e53f8ef9496d52ba15b54310124398cb8c0c70
Sadanandam Kanikaram <sada@sejalsoftware.com>
2021-04-23 12:19:43 -0700

    Updated message

--- GHFollowers/Utilities/ErrorMessage.swift
+++ GHFollowers/Utilities/ErrorMessage.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 enum GFError: String, Error {
-    case invalidUsername = "This user named created an invlid request. Please try again."
+    case invalidUsername = "SS This user named created an invlid request. Please try again."
     case unableToComplete = "Unable to complete your request. Please check your internet connection."
     case invalidResponse = "Invalid response from the server. Plase try again."
     case invalidData = "The data received from the server was invalid. Please try again."
